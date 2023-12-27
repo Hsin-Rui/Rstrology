08fee57e4a513ae8d155991ac9ed143012434690
Hsin-Rui <hsinrui.lin@gmail.com>
2023-12-27 02:49:36 +0100

    try to make CICD work (second try)

--- deploy/deploy-shinyapps.R
+++ deploy/deploy-shinyapps.R
@@ -1,7 +1,7 @@
 # usethis::use_build_ignore("deploy")
 
-rsconnect::setAccountInfo(name=Sys.getenv("SHINYAPPS_ACCOUNT"), 
-                          token=Sys.getenv('SHINYAPPS_TOKEN'), 
-                          secret=Sys.getenv("SHINYAPPS_SECRET"))
+rsconnect::setAccountInfo(name='astrocalculator', 
+                          token='40FB3997806249234B29E05ADD66D1E5', 
+                          secret='iOEbESXhdU0VaW4yQnOajRQW0XvNkIQgY/M1aBIk')
 
 rsconnect::deployApp(appName = "Rstrology")
