fde66f9121d0fed8f6d7602a50366d3d749274c6
Hsin-Rui <hsinrui.lin@gmail.com>
2023-12-30 22:13:00 +0100

    cities data documentation

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Cities data
+#'
+#' Cities having population > 1000 along with country name. Latitude and longitude are included in names for astrological calculation.
+#' 
+#'
+#' @format ## `cities`
+#' A data frame with 145,536 rows and 2 columns:
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{city}{name of city, lat: latitude, lng: longitude.}
+#'   
+#' }
+#' @source <https://www.geonames.org/>
+#' @source <https://api.opencube.tw/twzipcode>
+"cities"
