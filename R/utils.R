941bd3ec558df62a073834cb1f5b50aa3f49ed5f
Hsin-Rui <37245651+Hsin-Rui@users.noreply.github.com>
2024-05-20 21:37:49 +0200

    Finished debugging for visulization of planetary position in chart

--- R/utils.R
+++ R/utils.R
@@ -125,22 +125,37 @@ convert_planet_symbol <- function(x){
 #' @return a numeric vector of theta
 #' 
 
-adjust_planet_theta <- function(theta, adjust_distance=500, steps=90) {
+adjust_planet_theta <- function(theta, adjust_distance=650, steps=90) {
   
   for (i in 1:length(theta)) {
     
-    distance <- theta[i] - theta
-    distance [distance==0] <- Inf
+    distance <- determine_distance(theta, i=i)
+      
+    while(TRUE %in% (distance==0)) {
+      
+      theta [distance == 0] <- theta [distance==0] + steps
+      distance <- determine_distance(theta, i=i)
+      
+    }
+    
     needs_adjustment <-  dplyr::between(abs(distance), rep(0, length(distance)), rep(adjust_distance, length(distance)))
     should_continue <- TRUE %in% needs_adjustment
     
     while(should_continue) {
       
-      theta  [needs_adjustment & distance > 0] <- theta  [needs_adjustment & distance > 0] - steps # planets on the left move to left
-      theta  [needs_adjustment & distance < 0] <- theta  [needs_adjustment & distance < 0] + steps # planets on the right move to right
+      if(dplyr::between(theta[i], 0, 3000)) {
+        
+        theta [needs_adjustment] <- theta [needs_adjustment] + steps # for planets in 7th house, always move to right
+        
+      } else {
+        
+        theta  [needs_adjustment & distance > 0] <- theta  [needs_adjustment & distance > 0] - steps # planets on the left move to left
+        theta  [needs_adjustment & distance < 0] <- theta  [needs_adjustment & distance < 0] + steps # planets on the right move to right
+        
+      }
       
       distance <- theta[i] - theta
-      distance [distance==0] <- Inf
+      distance [i] <- Inf
       needs_adjustment <-  dplyr::between(abs(distance), rep(0, length(distance)), rep(adjust_distance, length(distance)))
       should_continue <- TRUE %in% needs_adjustment
       
@@ -160,14 +175,14 @@ adjust_planet_theta <- function(theta, adjust_distance=500, steps=90) {
 #' @return a signle logical value (TRUE = still needs revision)
 #' 
 
-examine_distance <- function(theta, criteria=500){
+examine_distance <- function(theta, criteria=550){
   
   res <- rep(NA, length(theta))
   
   for (i in 1:length(theta)) {
     
     distance <- theta[i]-theta
-    distance [distance==0] <- Inf
+    distance [i] <- Inf
     res[i] <- TRUE %in% (abs(distance) < criteria)
       
   }
@@ -178,52 +193,57 @@ examine_distance <- function(theta, criteria=500){
 
 #' Optimize until all planets are not overlapping
 #' 
-#' @param planet_theta a numeric vector
-#' @param selected_elements all planets selected for chart
+#' @param theta a numeric vector
+#' @param planets all planets selected for chart
 #'
 
-optmize_planet_position <- function(planet_theta, selected_elements){
+optmize_planet_position <- function(theta, planets){
   
-  original_order <- order(planet_theta)
+  ## two methods are used to optimize planetary positions
   
-  new_theta <- adjust_planet_theta(planet_theta)
+  names(theta) <- convert_planet_symbol(planets)
+  original_order <- order(theta)
+  
+  ## 1. this is the first method: starts from the sun, looks for planets that are too close, move them away little by little in iteration
+  new_theta <- adjust_planet_theta(theta)  
   should_continue <- examine_distance(new_theta)
-  
+
   m <- 0
-  while(should_continue & m < 4) {
+  while(isTRUE(should_continue) & m < 5) {
     
     new_theta <- adjust_planet_theta(new_theta)
     should_continue <- examine_distance(new_theta)
-    
-    m <- m + 1
-  }
-  
-  m <- 0
-  while(should_continue  & m < 4) {
-    
-    new_theta <- adjust_planet_theta(new_theta, steps = 130)
-    should_continue <- examine_distance(new_theta)
-    
     m <- m + 1
     
   }
   
-  m <- 0
-  while(should_continue  & m < 4) {
-    
-    new_theta <- adjust_planet_theta(new_theta, steps = 150)
-    should_continue <- examine_distance(new_theta)
-    
-    m <- m + 1
-    
-  }
-  
+  ## after that, some normalization is done, so that values are within range (0-36000)
   new_theta <- new_theta [order(new_theta)]
-  names(new_theta) <- selected_elements [original_order]
+  names(new_theta) <- planets [original_order]
   names(new_theta) <- convert_planet_symbol(names(new_theta))
+  new_theta <- normalize_theta(new_theta)
   
-  if(TRUE %in% (new_theta < 0)) new_theta [new_theta < 0] <- new_theta [new_theta < 0] + 36000
-  if(TRUE %in% (new_theta > 36000)) new_theta [new_theta > 36000] <- new_theta [new_theta > 36000] - 36000
+  current_order <- names(new_theta)
+  should_continue <- examine_distance(new_theta)
+  
+  ## 2. second method: looks for Stellium, normalize the planets so that they have 600theta distance
+  m <- 0
+  while(isTRUE(should_continue) & m < 20) {
+     
+    new_theta <- rescale_planet_theta(new_theta)
+    should_continue <- examine_distance(new_theta)
+    m <- m+1
+    
+  }
+  
+  ## Final check, when planets are in 6 & 7 house, there could be problems (0 degree & 359 degree are next to each other)
+  has_western_planets <- (TRUE %in% (new_theta < 1000)) & (TRUE %in% (new_theta > 35000))
+  
+  if (isTRUE(has_western_planets)) {
+    new_theta <- reorder_west_planets(theta, new_theta)
+  }
+  
+  new_theta <- new_theta [match(current_order, names(new_theta))]
   
   return(new_theta)
   
@@ -282,3 +302,129 @@ minus_datetime <- function(start_time, unit, value){
   
 }
 
+#' Normalize theta (ensure theta within range after optimization)
+#' 
+#' @param theta a numeric vector (integer)
+#'
+
+normalize_theta <- function(theta) {
+  
+  if(TRUE %in% (theta < 0)) theta [theta < 0] <- theta [theta < 0] + 36000
+  if(TRUE %in% (theta > 36000)) theta [theta > 36000] <- theta [theta > 36000] - 36000
+  
+  return(theta)
+  
+}
+
+#' Determine theta distance
+#' 
+#' @param x a vector of theta
+#' @param i iteration index
+#' 
+
+determine_distance <- function(x, i) {
+  
+  distance <- x[i] - x
+  distance [i] <- Inf
+  
+  return(distance)
+}
+
+#' Calculate distance difference of a theta string (prior element - subsequent element)
+#' @param x a vector of theta (must be sorted in order)
+#' 
+
+calculate_difference <- function(x){
+  
+  distance <- x- dplyr::lead(x)
+  distance[is.na(distance)] <- 0
+  
+  return(distance)
+  
+}
+
+#' Optimize theta by re-distributing space equally (applied mainly on stellium)
+#' @param x a vector of theta (must be sorted in order)
+#' 
+
+rescale_planet_theta <- function(x){
+  
+  x <- sort(x)
+  distance <- calculate_difference(x)
+  distance[length(distance)] <- Inf
+  
+  # get the first planet distance smaller than 600
+  position <- min(which(abs(distance) < 600))
+  selected <- rep(FALSE, length(x))
+  
+  for(i in (position):length(x)){
+    
+    result <- abs(x[i] - x[i+1]) < 750
+    selected[i] <- result
+    if(!isTRUE(result)) break 
+   
+  }
+  
+  selected[i] <- TRUE
+  
+  if(position > 1) {
+    
+    for(i in(position):2){
+      
+      result <- abs(x[i] - x[i-1]) < 750
+      selected[i-1] <- result
+      if(!isTRUE(result)) break 
+      
+    }
+    
+  }
+  
+  objects_of_interest <- x[selected]
+
+
+  for (i in 2:(length(objects_of_interest))){
+      objects_of_interest[i] <- objects_of_interest[i-1] + 600
+  }
+  
+  x [which(names(x) %in% names(objects_of_interest))] <- objects_of_interest
+  return(x)
+}
+
+#' Take original theta and manipulated theta, reorder to ensure planets are displayed in correct order
+#' 
+#' @param theta original theta vector
+#' @param new_theta manipulated theta vector
+#' 
+#' @importFrom dplyr between, case_when
+#' 
+
+reorder_west_planets <- function(theta, new_theta){
+  
+  original <- dplyr::case_when(dplyr::between(theta, 0, 3000) ~ theta + 36000, TRUE ~ theta)
+  new <- dplyr::case_when(dplyr::between(new_theta, 0, 3000) ~ new_theta + 36000, TRUE ~ new_theta)
+  needs_rescale <- examine_distance(new)
+  
+  if(isTRUE(needs_rescale)) new <- rescale_planet_theta(new)
+  
+  original_order <- sort(original)
+  new_order <- sort(new)
+  
+  start_from <- which(names(new_order) %in% names(original_order)[1])
+  if(start_from != 1) {
+    position <- c(start_from:length(original_order), 1:(start_from)-1)
+    new_order <- new_order[position]
+  }
+  
+  needs_reorder <- FALSE %in% (names(original_order) == names(new_order))
+  
+  if(isTRUE(needs_reorder)) {
+    
+    original <- sort(original)
+    new <- sort(new)
+    names(new) <- names(original)
+    
+  }
+  
+  new <- dplyr::case_when(new > 36000 ~ new - 36000, TRUE ~ new)
+  return(new)
+}
