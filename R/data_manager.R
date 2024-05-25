aa951d2361a261597a5343fbbd174b1f1082a2c3
Hsin-Rui <37245651+Hsin-Rui@users.noreply.github.com>
2024-05-25 22:35:14 +0200

    Put on chart information legend. Fix bug in date_to_jd() .. timezone issues fixed

--- R/data_manager.R
+++ R/data_manager.R
@@ -22,12 +22,18 @@ DataManager <- R6::R6Class(
     #' @field horoscope_city (`character()`)\cr
     #' City with longitude & latitude. Default to Taipei City
     horoscope_city = NULL,
+    #' @field horoscope_country (`character()`)\cr
+    #' Country
+    horoscope_country = NULL,
     #' @field planet_position (`list()`)\cr
     #' A list of dataf rame containing planetary positions and house cusps
     planet_position = NULL,
     #' @field chart (`list()`)\cr
     #' #' ggplot object
     chart = NULL,
+    #' @field chart_name (`character()`)\cr
+    #' name of the chart
+    chart_name = "Transits",
     
     #' @description
     #' Initalize the R6 class using the original shiny.i18n Translator class initialize function
@@ -39,6 +45,7 @@ DataManager <- R6::R6Class(
     initialize = function(translation_csvs_path = "./inst/csv/", separator_csv="|"){
       super$initialize(translation_csvs_path=translation_csvs_path, separator_csv=separator_csv)
       self$horoscope_city <- cities$city [1]
+      self$horoscope_country <- cities$country [cities$city %in% self$horoscope_city]
       self$update_chart()
     },
     
@@ -49,7 +56,7 @@ DataManager <- R6::R6Class(
       self$planet_position <- calculate_planet_position(self$horoscope_datetime, self$horoscope_timezone, self$horoscope_city)
       data <- self$planet_position$planetary_position
       data <- data[!(row.names(data) %in% "true_node"),]
-      self$chart <- draw_whole_sign_chart(data)
+      self$chart <- draw_whole_sign_chart(data, self$chart_name, self$horoscope_datetime, self$horoscope_city, self$horoscope_country)
     }
   )
 )
