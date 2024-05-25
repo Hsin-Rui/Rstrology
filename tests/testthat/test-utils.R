fa97a9979bd4b25b4d672f432602787617856878
Hsin-Rui <37245651+Hsin-Rui@users.noreply.github.com>
2024-05-25 22:46:03 +0200

    updated text expectation

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,5 +1,5 @@
 test_that("jd transformation works", {
   expect_equal(date_to_jd(lubridate::ymd_hm("1986-01-01 20:20")), 2446432)
   expect_equal(date_to_jd(lubridate::ymd_hm("2000-05-30 12:00")), 2451694.7)
-  expect_equal(date_to_jd(lubridate::ymd_hm("2030-09-16 16:20")), 2462760.8)
+  expect_equal(date_to_jd(lubridate::ymd_hm("2030-09-16 16:20")), 2462760.85)
 })
